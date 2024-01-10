9b762537bb7750ee80d4b0035a87d64087cbb0c0
Gandort <oluwasemiloreakinnuli@gmail.com>
2024-01-10 12:32:08 +0100

    Add Footer and Update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
   return (
     <nav className="w-full h-[60px] px-4 mt-4 bg-white opacity-25 hidden lg:flex justify-between text-lg font-medium rounded-xl">
         <ul className="flex items-center gap-[40px] text-[#3F64EA] text-opacity-[43%]">
-            <li>About</li>
-            <li>Contact</li>
-            <li>Tech Stack</li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/projects'>Projects</Link></li>
+            <li><Link to='/contact'>Contact</Link></li>
         </ul>
         <div className="flex items-center gap-[24px]">
             <img src="/assets/LinkedIn.png" alt="" />
