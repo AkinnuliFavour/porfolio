7569d72f62b21ec147abb18fdda45266c1fe6e94
Gandort <oluwasemiloreakinnuli@gmail.com>
2024-01-11 13:26:16 +0100

    Add About page content and images

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -16,8 +16,8 @@ const About = () => {
             </section>
             <hr className="w-full text-black opacity-10 mt-[60px]"/>
        </div>
-        <section className="px-[18px] lg:px-[244px] mt-[60px] flex items-center gap-[75px]">
-            <section className="">
+        <section className="px-[18px] lg:px-[244px] mt-[60px] flex flex-col-reverse lg:flex-row items-center gap-[75px]">
+            <section className="max-w-[358px] lg:w-[645px]">
                 <h3 className="text-[16px] font-semibold text-[#181818]">Who I am</h3>
                 <p className="text-[15px] mt-[9px]">I'm <span className="text-[#181818]">Favour</span>, a multi-disciplinary front-end engineer based in Akure, Nigeria.</p>
                 <h3 className="text-[16px] font-semibold text-[#181818] mt-[32px]">What I Do</h3>
@@ -26,11 +26,18 @@ const About = () => {
                 <p className="text-[15px] mt-[9px]">Before delving into the realm of front end engineering, I spent five years as a graphic designer. This background has equipped me with a keen eye for aesthetics and a deep understanding of user-centered design principles. It enables me to seamlessly blend functionality and visual appeal in every project I undertake.</p>
                 <p className="text-[18px] mt-[60px]">Feel free to reach out via e-mail, or follow me on Twitter. Want to see where I’ve worked? Check out my Resume, or Connect with me on LinkedIn.</p>
             </section>
-            {/* <section>
-                <img src="" alt="" />
-                <button>Get in touch</button>
-            </section> */}
+            <section className="flex flex-col justify-center items-center">
+              <section className="h-[322.28px] lg:h-[443px] bg-black lg:w-[344px] w-[250px] px-[44px] pt-[68.9px] pb-[68.3px] rounded-[10.95px]">
+                  <img src="/assets/favour.jpg" alt="" className="w-[256.19px] rounded-full"/>
+              </section>
+              <div className="flex items-center lg:w-[201px] mt-[34px] lg:mt-[100px]">
+                <button className="bg-gradient-to-r from-black to-black w-full py-[16px] px-[32px] rounded-[9px] text-white hidden lg:block"> Get in touch</button>
+              </div>
+            </section>
         </section>
+        <div className="flex items-center w-full mt-[100px] px-[16px]">
+          <button className="bg-gradient-to-r from-black to-black w-full py-[16px] px-[32px] rounded-[9px] text-white lg:hidden"> Get in touch</button>
+        </div>
         <Footer />
     </main>
   )
