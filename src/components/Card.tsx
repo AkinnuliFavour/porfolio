7a7c71aae7f69c83a90161f1a3bc2762da28c6bc
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-09-20 22:53:53 +0000

    Refactor Card component and remove unused import

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import FrontCard from "./FrontCard";
 // import { NewFrontCard } from "./FrontCard";
-import BackCard from "./BackCard";
 
 interface CardProps {
   name: string;
