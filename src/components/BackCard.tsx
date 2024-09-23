331a7c8b44b1c8cb9627e07a8bea38d20f35c554
Akinnuli Favour <oluwasemiloreakinnuli@gmail.com>
2024-09-23 05:44:20 +0000

    Refactor BackCard component styles

--- src/components/BackCard.tsx
+++ src/components/BackCard.tsx
@@ -9,7 +9,7 @@ interface BackCardProps {
 const BackCard = ({ tags, isHovered }: BackCardProps) => {
   return (
 
-    <div className={`absolute bg-black rounded-full px-20 h-96 w-full bg-opacity-80 text-[#999] font-bold flex justify-center items-center transition-all duration-300 ease-in-out ${isHovered ? 'opacity-100 scale-100' : 'opacity-0 scale-110'}`}>
+    <div className={`absolute bg-black rounded-[32px] px-20 h-96 w-full bg-opacity-80 text-[#999] font-bold flex justify-center items-center shadow-md shadow-gray-500 transition-all duration-300 ease-in-out ${isHovered ? 'opacity-100 scale-100' : 'opacity-0 scale-110'}`}>
       <div
         className="w-4/5 h- flex justify-center items-center gap-10 flex-wrap opacity-100"
         style={{ opacity: 1 }}
