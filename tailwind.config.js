54aafba24be933bd1369fa004321c21cc6ed5e1d
AkinnuliFavour <oluwasemiloreakinnuli@gmail.com>
2026-01-20 15:50:55 +0100

    Refactor assets and update Tailwind CSS configuration with new colors, shadows, and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require("tailwindcss/plugin");
+import plugin from "tailwindcss/plugin";
 
 const CustomStyle = plugin(function ({ addUtilities }) {
   addUtilities({
@@ -22,7 +22,32 @@ const CustomStyle = plugin(function ({ addUtilities }) {
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "text-primary": "#181818",
+        "text-secondary": "#808080",
+        "bg-light": "#F9F9F9",
+        "bg-card": "#F6F6F6",
+        "tag-bg": "#313131",
+        "tag-border": "#222",
+      },
+      boxShadow: {
+        "card-default":
+          "0 4px 6px rgba(0, 0, 0, 0.07), 0 1px 3px rgba(0, 0, 0, 0.06)",
+        "card-hover":
+          "0 20px 25px rgba(0, 0, 0, 0.1), 0 10px 10px rgba(0, 0, 0, 0.04)",
+        tag: "0 2px 4px rgba(0, 0, 0, 0.3)",
+      },
+      animation: {
+        "fade-in": "fadeIn 300ms ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+    },
   },
   plugins: [CustomStyle],
 };
