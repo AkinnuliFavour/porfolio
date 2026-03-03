4670bb9d441ea7f6d8cf06df4a08a6b9de1132f5
AkinnuliFavour <oluwasemiloreakinnuli@gmail.com>
2026-03-03 10:31:50 +0100

    Add profile image and update About section for consistency

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -11,50 +11,49 @@ const About = () => {
   }, []);
 
   return (
-    <main className="text-[#808080]">
+    <main className='text-[#808080]'>
       <div className="bg-[url('/assets/background-gradient-top.png')] bg-center w-full flex flex-col">
-        <section className="px-[18px] lg:px-[244px]">
+        <section className='px-[18px] lg:px-[244px]'>
           <Navbar />
         </section>
         <Link
-          to="/"
-          className="flex justify-center mb-[86px] mt-[32px] lg:hidden"
+          to='/'
+          className='flex justify-center mb-[86px] mt-[32px] lg:hidden'
         >
-          <img src="/assets/favour-logo-white.svg" alt="" />
+          <img src='/assets/favour-logo-white.svg' alt='' />
         </Link>
-        <section className="px-[16px] lg:px-[244px] lg:mt-[121px]">
-          <h2 className="text-[48px] lg:text-[70px] font-extrabold text-[#181818]">
+        <section className='px-[16px] lg:px-[244px] lg:mt-[121px]'>
+          <h2 className='text-[48px] lg:text-[70px] font-extrabold text-[#181818]'>
             A little bit about me
           </h2>
-          <p className="text-[15px] lg:text-[24px] font-normal">
+          <p className='text-[15px] lg:text-[24px] font-normal'>
             Who am I and what do I do?
           </p>
         </section>
-        <hr className="w-full text-black opacity-10 mt-[60px]" />
       </div>
-      <section className="px-[18px] lg:px-[244px] gap-[75px]">
-        <section className="flex flex-col justify-center items-center">
-          <section className="h-[322.28px] lg:h-[400px] lg:w-[344px] w-[250px] px-[44px] pt-[68.9px] pb-[68.3px] rounded-[10.95px]">
+      <section className='px-[18px] lg:px-[244px] gap-[75px]'>
+        <section className='flex flex-col justify-center items-center'>
+          <section className='lg:w-[344px] w-[250px] px-[44px] pt-[30px] pb-[30px] rounded-[10.95px]'>
             <img
-              src="/assets/favour.jpg"
-              alt=""
-              className="w-[256.19px] rounded-full"
+              src='/assets/profile.jpeg'
+              alt=''
+              className='w-[256.19px] rounded-full'
             />
           </section>
         </section>
-        <section className="w-full">
-          <h3 className="text-[16px] font-semibold text-[#181818]">Who I am</h3>
-          <p className="text-[15px] mt-[9px]">
+        <section className='w-full'>
+          <h3 className='text-[16px] font-semibold text-[#181818]'>Who I am</h3>
+          <p className='text-[15px] mt-[9px]'>
             I'm{" "}
-            <span className="text-[#181818]">
+            <span className='text-[#181818]'>
               Akinnuli Favour Oluwasemilore
             </span>
             , a multi-disciplinary front-end engineer based in Akure, Nigeria.
           </p>
-          <h3 className="text-[16px] font-semibold text-[#181818] mt-[32px]">
+          <h3 className='text-[16px] font-semibold text-[#181818] mt-[32px]'>
             A Brief Introduction
           </h3>
-          <p className="text-[15px] mt-[9px]">
+          <p className='text-[15px] mt-[9px]'>
             I am a passionate Frontend Engineer with a strong foundation in
             HTML, CSS, and JavaScript, complemented by expertise in modern
             frameworks like React and frontend utility libraries like Tailwind
@@ -68,19 +67,19 @@ const About = () => {
             and a knack for problem-solving, I strive to leverage my skills to
             craft engaging digital experiences that make a meaningful impact.
           </p>
-          <p className="text-[18px] mt-[60px]">
+          <p className='text-[18px] mt-[60px]'>
             Feel free to reach out via e-mail, or follow me on Twitter. Want to
             see where I’ve worked? Check out my Resume, or Connect with me on
             LinkedIn.
           </p>
         </section>
       </section>
-      <div className="flex items-center w-full mt-[100px] px-[16px] lg:px-[244px]">
+      <div className='flex items-center w-full mt-[100px] px-[16px] lg:px-[244px]'>
         <button
-          className="w-full px-8 py-4 hover:before:bg-black relative overflow-hidden border border-black bg-white text-black shadow-2xl rounded-lg transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-500 hover:text-white hover:shadow-black hover:before:left-0 hover:before:w-full"
+          className='w-full px-8 py-4 hover:before:bg-black relative overflow-hidden border border-black bg-white text-black shadow-2xl rounded-lg transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-black before:transition-all before:duration-500 hover:text-white hover:shadow-black hover:before:left-0 hover:before:w-full'
           onClick={() => navigate("/contact")}
         >
-          <span className="relative z-10">Get in touch</span>
+          <span className='relative z-10'>Get in touch</span>
         </button>
       </div>
       <Footer />
